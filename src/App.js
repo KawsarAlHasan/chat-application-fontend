e33a3676569f6f0fa3d35f1c8c16a512df60aad9
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2022-11-17 03:11:57 +0600

    added navbar and routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Navigation from './components/Navigation'
+import Home from './pages/Home'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+import Chat from './pages/Chat'
 
 function App() {
-  return <div>hello</div>
+  return (
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
