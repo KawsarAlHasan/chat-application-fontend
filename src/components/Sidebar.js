559b6816ea3e118617c11c86511fed9ac686c5d7
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2023-01-04 01:29:54 +0600

    chack git push and try to resolve

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -35,7 +35,7 @@ function Sidebar() {
   })
 
   function getRooms() {
-    fetch('https://chat-application-server-k4ks.onrender.com/rooms')
+    fetch('http://localhost:5001/rooms')
       .then((res) => res.json())
       .then((data) => setRooms(data))
   }
--- src/services/appApi.js
+++ src/services/appApi.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 const appApi = createApi({
   reducerPath: 'appApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://chat-application-server-k4ks.onrender.com',
+    baseUrl: 'http://localhost:5001',
   }),
 
   endpoints: (builder) => ({
